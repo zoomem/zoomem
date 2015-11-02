2c2faa6de5be79dff10d07cc8ad95ee760befc96
Tariq Khundakji <tariqkhundakji@gmail.com>
2015-11-02 11:41:28 +0200

    fix syntax errors in main.py and graph.cpp

--- main.py
+++ main.py
@@ -4,36 +4,30 @@ import hashlib
 import non_blocking_stream
 import source_parsing
 
-p = Popen('gdb ~/Desktop/py/test',stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = True)
-functionsNameList = source_parsing.getFunctionsNames('text.txt')
+name_to_address_hash = {}
 
-nbsr = non_blocking_stream.NonBlockingStreamReader(p.stdout)
-
-for name in functionsNameList:
-    p.stdin.write('b ' + name + '\n')
-
-varHash = {}
-def getVaribleAddress(var_name):
+def getVarAddressByName(var_name):
     writeToGdb("p &"+var_name)
     address = praseGdbOutput("varAddress")
     last_index =  address.rfind(" ")
     if last_index != -1:
         address = address[lastIndex+1:]
     return address
-def updateVariableleHash(var_name):
 
-    var_address = getVaribleAddress(var_name)
-    varHash[var_name] = [] if not exists varHash{var_name};
-    varHash[var_name].append(var_address)
+def hashVarNameAdd(var_name):
+    var_address = getVarAddressByName(var_name)
+    if not name_to_address_hash.has_key(var_name):
+        name_to_address_hash[var_name] = []
+
+    name_to_address_hash[var_name].append(var_address)
 
 def checkScope(gdb_output):
-
     lastIndex = 0
     info_local_lines = gdb_output.split("\n")
     #print str
     for i in range(0,len(info_local_lines)):
-        var_name = info_local_lines[i].split(':').first.strip()
-        var_adcheckVarible(var_name)
+        var_name = info_local_lines[i].split(':')[0].strip()
+        hashVarNameAdd(var_name)
 
 def readGdbOutput():
     temp = ""
@@ -46,7 +40,6 @@ def readGdbOutput():
 
 def praseGdbOutput(query):
     temp = readGdbOutput()
-    #print "out : " + temp
     if query == "print":
         print temp
         return
@@ -59,12 +52,20 @@ def praseGdbOutput(query):
 def writeToGdb(command):
     if(command == "exit"):
         return
-    p.stdin.write(command + '\n')
+    gdp_process.stdin.write(command + '\n')
 
-varHash = {}
+
+gdp_process = Popen('gdb test -q',stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = True)
+nbsr = non_blocking_stream.NonBlockingStreamReader(gdp_process.stdout)
+
+for function_name in source_parsing.getFunctionsNames('test.cpp'):
+    gdp_process.stdin.write('b ' + function_name + '\n')
+
 writeToGdb("run")
 praseGdbOutput("print")
+
 while True:
+    print readGdbOutput()
     command = raw_input()
     writeToGdb(command)
     praseGdbOutput("print")
--- test.cpp
+++ test.cpp
@@ -3,14 +3,9 @@
 using namespace std;
 
 int main(){
-	
+
 	int x = 1;
 	x = 1 + 2;
 	int y = 2;
 	y = 1 + 2;
-	/*node* head1 = new node(5,NULL);
-	node* head2 = new node(6,head1);
-	node* head3 = new node(7,head2);
-	node* head4 = new node(8,head3);
-	cout << head4->val;*/
 }
