f946d427158048eb12495feca1d99f7f904420ca
Tariq Khundakji <tariqkhundakji@gmail.com>
2015-11-03 19:06:53 +0200

    Added code to detect arrays and pointers
    parsing will be added soon
    fixed a bug where the adress was not retrived, now it works
    code clean_up

--- main.py
+++ main.py
@@ -4,71 +4,86 @@ import hashlib
 import non_blocking_stream
 import source_parsing
 
-name_to_address_hash = {}
+address_dict = {}
+gdb_process = Popen('gdb test -q',stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = True)
+gdb_process_nbsr = non_blocking_stream.NonBlockingStreamReader(gdb_process.stdout)
 
-def getVarAddressByName(var_name):
-    writeToGdb("p &"+var_name)
-    address = praseGdbOutput("varAddress")
-    last_index =  address.rfind(" ")
-    if last_index != -1:
-        address = address[lastIndex+1:]
-    return address
+def getVarAddress(var_name):
+    writeToProcess(gdb_process,"p &" + var_name)
+    var_address = praseGdbOutput()
+    return var_address[var_address.rfind(" ") + 1:]
 
-def hashVarNameAdd(var_name):
-    var_address = getVarAddressByName(var_name)
-    if not name_to_address_hash.has_key(var_name):
-        name_to_address_hash[var_name] = []
+def getVarType(var_name):
+    writeToProcess(gdb_process,"ptype "+ var_name)
+    var_type = praseGdbOutput()
+    return var_type[var_type.rfind("=") + 1:]
 
-    name_to_address_hash[var_name].append(var_address)
-
-def checkScope(gdb_output):
-    lastIndex = 0
-    info_local_lines = gdb_output.split("\n")
-    #print str
+def getLocalVariablesName():
+    writeToProcess(gdb_process,"info locals")
+    var_names = []
+    info_local_lines = readProcessOutput(gdb_process_nbsr).split("\n")
     for i in range(0,len(info_local_lines)):
-        var_name = info_local_lines[i].split(':')[0].strip()
-        hashVarNameAdd(var_name)
+        var_name = info_local_lines[i].split('=')[0].strip()
+        var_names.append(var_name)
+    return var_names
 
-def readGdbOutput():
-    temp = ""
+def is_a_pointer(str):
+    return str[str.rfind(" ") + 1:][0] == "*"
+
+def is_a_object(str):
+    return False
+
+def is_a_array(str):
+    return str[str.rfind(" ") + 1:][0] == "["
+
+def checkScope():
+    var_names = getLocalVariablesName()
+    for var_name in var_names:
+        addVarNameToDic(var_name)
+
+def addVarNameToDic(var_name):
+    var_address = getVarAddress(var_name)
+    if not address_dict.has_key(var_name):
+        address_dict[var_name] = []
+    if not var_address in address_dict[var_name]:
+        address_dict[var_name].append(var_address)
+
+def praseGdbOutput(query = ""):
+    output = readProcessOutput(gdb_process_nbsr)
+    if query == "print":
+        print output
+    else:
+        return output
+
+def readProcessOutput(nbsr):
+    output_lines = ""
     while True:
         output = nbsr.readline(0.2)
         if not output:
             break
-        temp += output
-    return temp
+        output_lines += output
+    return output_lines.strip()
 
-def praseGdbOutput(query):
-    temp = readGdbOutput()
-    if query == "print":
-        print temp
-        return
-    if query == "scope":
-        checkScope(temp)
-        return
-    if query == "varAddress":
-        return temp
+def writeToProcess(process,command):
+    process.stdin.write(command + '\n')
 
-def writeToGdb(command):
-    if(command == "exit"):
-        return
-    gdp_process.stdin.write(command + '\n')
+def main():
+    for function_name in source_parsing.getFunctionsNames('test.cpp'):
+        writeToProcess(gdb_process,('b ' + function_name + '\n'))
 
+    writeToProcess(gdb_process,"run")
+    praseGdbOutput()
 
-gdp_process = Popen('gdb test -q',stdin = PIPE, stdout = PIPE, stderr = PIPE, shell = True)
-nbsr = non_blocking_stream.NonBlockingStreamReader(gdp_process.stdout)
+    while True:
+        command = raw_input()
+        writeToProcess(gdb_process,command)
+        praseGdbOutput("print")
+        var_names = getLocalVariablesName()
 
-for function_name in source_parsing.getFunctionsNames('test.cpp'):
-    gdp_process.stdin.write('b ' + function_name + '\n')
+        for var_name in var_names:
+            var_type =  getVarType(var_name)
+            print var_type
+            print is_a_array(var_type)
+            print is_a_pointer(var_type)
 
-writeToGdb("run")
-praseGdbOutput("print")
-
-while True:
-    print readGdbOutput()
-    command = raw_input()
-    writeToGdb(command)
-    praseGdbOutput("print")
-    command = "info locals"
-    writeToGdb(command)
-    praseGdbOutput("scope")
+main()
--- test.cpp
+++ test.cpp
@@ -2,10 +2,22 @@
 #include<stdio.h>
 using namespace std;
 
+class a
+{
+	public:
+	int c,d;
+	a()
+	{
+		c = d = 0;
+	}
+};
 int main(){
 
-	int x = 1;
-	x = 1 + 2;
-	int y = 2;
-	y = 1 + 2;
+	int x = 5;
+	int *p = &x;
+	int **pp = &p;
+	int c[5] = {0,1,2,3,4};
+	unsigned long long cc = 45464;
+	a b;
+	return 0;
 }
