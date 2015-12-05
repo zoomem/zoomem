b9b34f7bb2fa7694e16480af4ecd00fb16134595
Tariq Khundakji <tariqkhundakji@gmail.com>
2015-12-05 12:23:02 +0200

    changed setuo.sh

--- zoomem_site/dev/setup.sh
+++ zoomem_site/dev/setup.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/gdb.py"
 DIR2="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.gdbinit"
-rm  /usr/share/gdb/python/gdb/zoomem
+rm -R /usr/share/gdb/python/gdb/zoomem
 rm ~/.gdbinit
 
 mkdir /usr/share/gdb/python/gdb/zoomem
--- zoomem_site/visualize/graph_py/sample.cpp
+++ zoomem_site/visualize/graph_py/sample.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
 	int x = 5;
-	int a[10][2]= {0};
 	int *p = &x;
 	return 0;
 }
