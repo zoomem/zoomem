0a07af80ca32d5ad091c1a9d71e7f114c22de91b
Tariq Khundakji <tariqkhundakji@gmail.com>
2015-12-09 01:27:51 +0200

    Fixed path problem

--- zoomem_site/visualize/views.py
+++ zoomem_site/visualize/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
 import sys
-sys.path.append("/home/tk/git_workplace/zoomem/zoomem_site/visualize/graph_py")
+sys.path.append("visualize/graph_py")
 from gdb_adapter import GdbAdapter
 from graph import gdbGraph
 import random, string
