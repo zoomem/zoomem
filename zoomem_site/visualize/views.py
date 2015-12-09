a7f1ec3c21d8304e335316a1e3493210111a97c5
Tariq Khundakji <tariqkhundakji@gmail.com>
2015-12-10 01:13:13 +0200

    Enabled input and texthighlging

--- zoomem_site/visualize/views.py
+++ zoomem_site/visualize/views.py
@@ -10,9 +10,8 @@ import os
 import time
 
 # Create your views here.
-def index(request,file_name = None):
-    path = "visualize/graph_py/"
-    g_adapter = GdbAdapter(file_name,path + "in.txt")
+def index(request,code_file_name = None,input_file_name = None):
+    g_adapter = GdbAdapter(code_file_name,input_file_name)
     g = g_adapter.bulidGraph(g_adapter.getGraphEdegs())
     edges = g.getGraphEdges()
     return render(request, 'visualize/index.html',{"edges":edges,"code":request.POST['code'].strip()})
@@ -22,17 +21,21 @@ def home(request):
 
 def submit(request):
     code = request.POST['code']
-    file_name = createFile(code)
-    compileFile(file_name)
-    return index(request,file_name)
+    inpt = request.POST['input']
+    code_file_name = createFile(code,".cpp")
+    input_file_name = createFile(inpt,".txt") + ".txt"
+    print code_file_name , input_file_name
+
+    compileFile(code_file_name)
+    return index(request,code_file_name,input_file_name)
 
 def randomword(length):
    return ''.join(random.choice(string.lowercase) for i in range(length))
 
-def createFile(code):
+def createFile(txt,exten):
     file_name = randomword(20)
-    f = open('visualize/static/cpp_files/' + file_name + ".cpp",'w')
-    f.write(code)
+    f = open('visualize/static/cpp_files/' + file_name + exten,'w')
+    f.write(txt)
     f.close()
     return 'visualize/static/cpp_files/' + file_name
 
