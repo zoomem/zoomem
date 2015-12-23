4bad3c258c0da344f2b83d4abf53e8a8aedcab0e
hp <tariqkhundakji@gmail.com>
2015-12-24 01:53:23 +0200

    hasan magic _1

--- zoomem_site/visualize/static/visualize/js/graph_bulider.js
+++ zoomem_site/visualize/static/visualize/js/graph_bulider.js
@@ -1,56 +1,152 @@
+var cvs,ctx,W,H;
+var NODEBGCOLOR="#ffffc6";
+var NODEBORDERCOLOR="#AAA";
+var nodes=[];
+var adj=[];
 var redraw;
-function drawGraph(edges,width) {
-
-    $("#drawArea").empty();
-    var height = width;
-    var render = function(r, n) {
-            var set = r.set().push(
-                r.rect(n.point[0]-30, n.point[1]-13, 60, 60).attr({ })).push(
-                r.text(n.point[0], n.point[1] + 15,n.id + "\n" + n.name + "\n" + n.type + "\n" + n.value + "\n"));
-            return set;
-        };
-
-    var render = function(r, n) {
-                /* the Raphael set is obligatory, containing all you want to display */
-                var set = r.set().push(
-                    /* custom objects go here */
-                    r.rect(n.point[0]-30, n.point[1]-13, 62, 66)).push(
-                    r.text(n.point[0], n.point[1] + 15,n.name).attr({"font-size":"14px"}));
-                /* custom tooltip attached to the set */
-                set./*tooltip = Raphael.el.tooltip;*/items.forEach(function(el) {
-                  el.tooltip(r.set().push(r.text(25, 35, "type "+  n.type + "\n" + "val "+  n.value + "\n").attr({"font-size":"14px"})))});
-
-    //            set.tooltip(r.set().push(r.rect(0, 0, 30, 30).attr({"fill": "#fec", "stroke-width": 1, r : "9px"})).hide());
-                return set;
-            };
-
-    var g = new Graph();
-
-    g.edgeFactory.build = function(source, target) {
-    	var e = jQuery.extend(true, {}, this.template);
-    	e.source = source;
-    	e.target = target;
-    	return e;
-    }
-
-    for(var i = 0; i < edges.length; ++i){
-      var src = edges[i][0]
-      var tar = edges[i][1]
-      var node = g.addNode(tar,{render:render});
-      node.name = edges[i][2];
-      node.address = edges[i][3]
-      node.type = edges[i][4]
-      node.size = edges[i][5]
-      node.value = edges[i][6]
-      node.flags =  edges[i][7]
-      if(src != 1)
-        edge = g.addEdge(src,tar);
-    }
-
-    var layouter = new Graph.Layout.Spring(g);
-    var renderer = new Graph.Renderer.Raphael('drawArea', g, width, height);
-    redraw = function() {
-        layouter.layout();
-        renderer.draw();
-    };
+function Node(o){
+  this.x=o.x;
+  this.y=o.y;
+  this.w=o.w;
+  this.h=o.h;
+  this.name=o.name;
 }
+var cam = {
+    x: 0,
+    y: 0,
+    cx: 0,
+    cy: 0,
+    z: 1,
+    cz: 1,
+};
+var mouse = {
+    down: false,
+    x: 0,
+    y: 0,
+    last: { x: 0, y: 0 },
+};
+function updateCamera() {
+    cam.cx += (cam.x - cam.cx) / 10;
+    cam.cy += (cam.y - cam.cy) / 10;
+    cam.cz += (cam.z - cam.cz) / 10;
+}
+Node.prototype.draw=function(){
+  ctx.fillStyle=NODEBGCOLOR;
+  ctx.fillRect(this.x,this.y,this.w,this.h);
+  ctx.lineWidth="1";
+  ctx.strokeStyle=NODEBORDERCOLOR;
+  ctx.strokeRect(this.x,this.y,this.w,this.h);
+  ctx.beginPath();
+  ctx.moveTo(this.x+6,this.y+23);
+  ctx.lineTo(this.x+this.w-5,this.y+23);
+  ctx.stroke();
+  ctx.fillStyle="black";
+  ctx.fillText(this.name,this.x+8,this.y+18);
+};
+var vis=[];
+var tree=[];
+function BFS(n){
+  var q=[],f=0,l=-1;
+  vis=[];
+  tree=[];
+  for(var i=0;i<n;++i){
+    q.push(0);
+    vis.push(0);
+    tree.push([]);
+  }
+  vis[0]=1;
+  q[++l]=0;
+  while(f<=l){
+    var u=q[f++];
+    for(var i=0;i<adj[u].length;++i)
+      if(vis[adj[u][i]]==0){
+        vis[adj[u][i]]=1;
+        tree[u].push(adj[u][i]);
+        q[++l]=adj[u][i];
+      }
+  }
+}
+function DFS(u,x,y){
+  if(tree[u].length==0){
+    nodes[u].x=x;
+    nodes[u].y=y;
+    return y;
+  }
+  for(var i=0;i<tree[u].length;++i){
+    y=DFS(tree[u][i],x+200,y);
+    if(i+1<tree[u].length)
+      y+=100;
+  }
+  y+=10;
+  nodes[u].x=x;
+  nodes[u].y=(nodes[tree[u][0]].y+nodes[tree[u][tree[u].length-1]].y)/2;
+  return y;
+}
+var first=1;
+var dataEdges,dataN;
+function drawGraph(edges,n) {
+  if(first==1){
+    dataEdges=edges;
+    dataN=n;
+    initialize();
+    first=0;
+  }
+  nodes=[];
+  adj=[];
+  for(var i=0;i<n;++i){
+    nodes.push(new Node({x:Math.random()*500,y:Math.random()*500,w:80,h:90,name:"Hasan"}));
+    adj.push([]);
+  }
+  for(var i=0;i<edges.length;++i){
+    nodes[edges[i][1]-1].name=edges[i][2];
+    adj[edges[i][0]-1].push(edges[i][1]-1);
+  }
+  BFS(n);
+  DFS(0,10,10);
+  for(var i=0;i<n;++i)
+    nodes[i].draw();
+}
+function draw(){
+  updateCamera();
+  ctx.clearRect(0,0,W,H);
+  ctx.save();
+  ctx.translate(cam.cx,cam.cy);
+  ctx.scale(cam.cz,cam.cz);
+  ctx.clearRect(0,0,W,H);
+  drawGraph(dataEdges,dataN);
+  ctx.restore();
+  ctx.fillStyle="black";
+  ctx.fillText(cam.cx+" , "+cam.cy,100,100);
+  requestAnimationFrame(draw);
+}
+var done=0;
+function initialize(){
+  if(done==1)
+    return;
+  done=1;
+  cvs=document.getElementById("cvs");
+  ctx=cvs.getContext("2d");
+  W=1280;
+  H=720;
+  cvs.onmousedown = function (e) {
+      mouse.down = true;
+      mouse.x = e.offsetX || (e.layerX-10);
+      mouse.y = e.offsetY || (e.layerY-10);
+      mouse.last = { x: mouse.x, y: mouse.y };
+  };
+  document.onmouseup = function (e) {
+      mouse.down = false;
+      mouse.last = { x: mouse.x, y: mouse.y };
+  };
+  document.onmousemove = function (e) {
+    if (mouse.down == false)
+        return;
+    var x = e.clientX - cvs.offsetLeft;
+    var y = e.clientY - cvs.offsetTop;
+    cam.x += (x - mouse.x);
+    cam.y += (y - mouse.y);
+    mouse.x = x;
+    mouse.y = y;
+  };
+  draw();
+};
